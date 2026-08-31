371e64900236e00e468809fc9edd5e0c1dce7642
Gopi Chand <gopi110531@gmail.com>
2026-08-31 13:05:43 -0500

    Roboshop code

--- 02-variable/sample.tf
+++ 02-variable/sample.tf
@@ -11,5 +11,54 @@ output "x1"{
     }
 
 output "x2"{
+    #for string
     value = "value of x1 is ${var.x1}"
     }
+
+#data types
+#string need to give in double quotes
+#number no need of quotes
+#boolean no need of quotes
+
+variable "string1"{
+    default = "abc"
+    }
+
+variable "num1"{
+    default = 100
+    }
+
+variable "num2"{
+    default = 100.2
+    }
+
+variable "boolean"{
+    default = true
+    }
+
+# Varibale types
+#plain
+#list
+#map
+
+variable "plain"{
+    default = "abc"
+    }
+
+variable "list"{
+    default = ["abc",123,true]
+    }
+
+variable "map"{
+    default = {
+        name = "devops"
+        skill = "engineer"
+        }
+    }
+
+output "list"{
+    value = var.list[1]}
+
+    output "map"{
+        value = var.map[skill]
+        }
