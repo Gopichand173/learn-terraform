d4656420611b1d0bf8714996d649f1e46b801bb2
Gopi Chand <gopi110531@gmail.com>
2026-08-31 18:33:28 -0500

    Roboshop code

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -1,3 +1,8 @@
 module "sample"{
     source = "./sample-module"
+    instance_type = var.instance_type
 }
+
+variable "instance_type"{
+    default = "abc"
+    }
--- 06-module/sample-module/demo.tf
+++ 06-module/sample-module/demo.tf
@@ -1,2 +1,11 @@
-resource "null_resource" "test"{
-    }
+resource "null_resource" "test"{}
+resource "aws_instance" "web" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "sample"
+
+ }
+|}
+variable "instance_type"  {}
