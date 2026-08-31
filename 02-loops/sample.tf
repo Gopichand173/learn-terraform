5179c96a238cc666e6a507019da893e5a9fe8718
Gopi Chand <gopi110531@gmail.com>
2026-08-31 16:08:49 -0500

    Roboshop code

--- 02-loops/sample.tf
+++ 02-loops/sample.tf
@@ -7,15 +7,16 @@ provider "aws" {}
 variable "instances"{
     default = {
        frontend = {
-       instance_type = "t3.micro"
+         instance_type = "t3.micro"
         }
-        catalogue = {
-              instance_type = "t3.micro"
+       catalogue = {
+         instance_type = "t3.micro"
                }
-    }}
+    }
+}
 
-resource "aws_instance" "instance" {
-    for_each = var.instances
+resource "aws_instance" "web" {
+  for_each      = var.instances
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = each.value["instance_type"]
 
