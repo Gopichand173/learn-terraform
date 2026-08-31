a58061b61ab9648c4f40a3d613b6b5b2ab2c293c
Gopi Chand <gopi110531@gmail.com>
2026-08-31 17:54:46 -0500

    Roboshop code

---
+++ conditions/sample.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = var.instance_type =="" ? "t3.micro" : var.instance_type
+
+  tags = {
+    Name = "sample"
+  }
+}
