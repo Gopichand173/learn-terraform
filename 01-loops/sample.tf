af20c4aa7de1b66792347203c60f3e335af7ddaa
Gopi Chand <gopi110531@gmail.com>
2026-08-31 14:14:57 -0500

    Roboshop code

--- 01-loops/sample.tf
+++ 01-loops/sample.tf
@@ -1,3 +1,19 @@
 resource "null_resource" "dummy"{
     count = 10
     }
+
+provider "aws" {}
+
+variable "instance"{
+    default = [frontend,catalogue]
+    }
+
+resource "aws_instance" "web" {
+    count = length(var.instance)
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.instance[count.index]
+  }
+}
